7d2e3224681b28837b03786d20a49db92fcc7681
Kristiqna <123325312+kristiqna-andonova@users.noreply.github.com>
2024-12-07 15:11:12 +0200

    Update settings.py

--- ask_a_woman/settings.py
+++ ask_a_woman/settings.py
@@ -31,7 +31,7 @@ DEBUG = os.getenv('DEBUG', config('DEBUG')) == 'True'
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', config('ALLOWED_HOSTS')).split(',')
 
-CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS', config('CSRF_TRUSTED_ORIGINS', [])).split(',')
+
 
 
 # Application definition
